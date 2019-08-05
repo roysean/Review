c214e8cf75bdec25bb4a925778c76897d93d535a
roysean <52671269+roysean@users.noreply.github.com>
2019-08-05 18:29:16 -0400

    Add files via upload

---
+++ assets/data/Members.ts
@@ -0,0 +1,19 @@
+import { Member } from '../../app/Member';
+
+export const MEMBER: Member[] = [
+  {
+    name: 'Sean',
+    vehicle: 'Car',
+    company: 'Royal Bank'
+  },
+  {
+    name: 'Mark',
+    vehicle: 'Bus',
+    company: 'Bandit Brewery'
+  },
+  {
+    name: 'Shawna',
+    vehicle: 'Train',
+    company: 'Halo Brewery'
+  }
+];
